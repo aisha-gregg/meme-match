5c4786cdfa0760087a859bb4958edd527f65baac
Aisha Gregg <aishagregg@gmail.com>
2020-04-14 17:46:04 +0200

    move code logic to game component

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,12 +1,44 @@
-import React from "react";
-import { Board } from "../components/Board";
+import React, { useEffect } from "react";
+import styles from "./game.module.css";
+import classNames from "classnames/bind";
 import { useState } from "react";
+import { MemeRepository } from "../components/MemeRepository";
+import { Card } from "./../components/Card";
+
+const cx = classNames.bind(styles);
 
 export function Game() {
-  const [cards, setCards] = useState();
-  const [canFlip, setCanFlip] = useState("false");
-  const [firstCard, setFirstCard] = useState();
-  const [secondCard, setSecondCard] = useState();
+  const [memes, setMemes] = useState([]);
 
-  return <Board></Board>;
+  useEffect(() => {
+    fetchMemes();
+  }, []);
+
+  async function fetchMemes() {
+    const memeRepository = new MemeRepository();
+    const memes = await memeRepository.findAll();
+    const duplicatedMemes = [...memes, ...memes];
+
+    const randomizedDuplicatedMemes = getRandomizedArray(duplicatedMemes);
+    setMemes(randomizedDuplicatedMemes);
+  }
+
+  return (
+    <div className={cx("gameBoard")}>
+      {memes.map((meme, index) => (
+        <Card key={index} image={meme.image}></Card>
+      ))}
+    </div>
+  );
+
+  // Passing values as reference
+  function getRandomizedArray(array) {
+    const arrayCopy = [...array];
+    for (let i = arrayCopy.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arrayCopy[i], arrayCopy[j]] = [arrayCopy[j], arrayCopy[i]];
+    }
+
+    return arrayCopy;
+  }
 }
